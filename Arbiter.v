2ccea8356fab2ede94a05e380e9efc91445abd8c
Nicholas McSween Manickchand <nicholasmcsweenmanickchand@gmail.com>
2026-02-19 12:49:25 -0500

    Base Arbiter finished

--- Arbiter.v
+++ Arbiter.v
@@ -1,31 +1,27 @@
-//clearly doesn't work for all inputs to all outputs, need to revisit and fix too tired to solve rn
-
-module Arbiter(
-    input [3:0] valid_port,
+module Arbiter( // 1 arbiter per gate so it only handles the message for it
     input [3:0] valid_input, // 0 is for A and 3 is for D 
     input [3:0] control_signal,
     input [7:0] rx_data [3:0], // all data paths
     input reset, clk,
-    output [7:0] tx_data [3:0] // all output paths
+    output [7:0] tx_data // outputs to the gate where the arbiter is places, (essentially making this complex MUX)
     // at 4 bits the compiler will know its not BRAM and use register so simply short-handing
 );
 
 // priority will be givn to port A to D initially, then ports will follow this order but only account for who passed the most recently
 
-localparam IDLE = 0, A = 1; B = 2, C = 3, D = 4;
+localparam IDLE = 0, A = 1, B = 2, C = 3, D = 4;
 reg [2:0] state, next_state;
-reg [2:0] target_port;
 
 always @(*) begin //no multicast support yet
     case (state)
         IDLE: begin
-            case (valid_port)
+            case (valid_input)
                 4'b0000: next_state = IDLE;
-                4'b0001: next_state = A
-                4'b0010: next_state = B
-                4'b0100: next_state = C
-                4'b1000: next_state = D
-                default: next_state = A // priority to A
+                4'b0001: next_state = A;
+                4'b0010: next_state = B;
+                4'b0100: next_state = C;
+                4'b1000: next_state = D;
+                default: next_state = A; // priority to A
             endcase
         end 
         A: begin
@@ -33,7 +29,7 @@ always @(*) begin //no multicast support yet
                 next_state = A;
             end
             else begin
-                case (valid_port) // currently can't handle if we stay on it for more than 1 data push
+                case (valid_input) // currently can't handle if we stay on it for more than 1 data push
                     4'b0000: next_state = IDLE; // notice how the current state isn't an option if control turns off
                     4'b0010: next_state = B;
                     4'b0100: next_state = C;
@@ -47,7 +43,7 @@ always @(*) begin //no multicast support yet
                 next_state = B;
             end
             else begin
-                case (valid_port)
+                case (valid_input)
                     4'b0000: next_state = IDLE;
                     4'b0001: next_state = A;
                     4'b0100: next_state = C;
@@ -61,7 +57,7 @@ always @(*) begin //no multicast support yet
                 next_state = C;
             end
             else begin
-                case (valid_port)
+                case (valid_input)
                     4'b0000: next_state = IDLE;
                     4'b0001: next_state = A;
                     4'b0100: next_state = C;
@@ -75,7 +71,7 @@ always @(*) begin //no multicast support yet
                 next_state = D;
             end
             else begin
-                case (valid_port)
+                case (valid_input)
                     4'b0000: next_state = IDLE;
                     4'b0001: next_state = A;
                     4'b0010: next_state = B;
@@ -83,40 +79,32 @@ always @(*) begin //no multicast support yet
                     default: next_state = A; // loops back around priority list
                 endcase
             end
+        default: next_state = IDLE;
     endcase
 
-    case(valid_port)
-        4'b0000: target_port = 3'h0;
-        4'b0001: target_port = 3'h1;
-        4'b0010: target_port = 3'h2;
-        4'b0100: target_port = 3'h3;
-        default: target_port = 3'h4; // broadcast;
 end
 
+always @(*) begin
+    case (state)
+            A: tx_data = rx_data[0]; //having them like this allows potentially for future multi-cast since multiple output streams?
+            B: tx_data = rx_data[1];
+            C: tx_data = rx_data[2];
+            D: tx_data = rx_data[3];
+            default: begin // basically IDLE
+                tx_data = 8'h0;
+            end
+        endcase
+end
 always @(posedge clk) begin
     if (reset) begin
         state <= IDLE;
-        tx_data[0] <= 8'h0;
-        tx_data[1] <= 8'h0;
-        tx_data[2] <= 8'h0;
-        tx_data[3] <= 8'h0;
+                          //tx_data <= 8'h0; not necessary here bc when state is idle does this automatically + multiple drivers not allowed
     end
     else begin
         state <= next_state;
-        case (state)
-            A: tx_data[0] <= rx_data[0]; //having them like this allows potentially for future multi-cast since multiple output streams?
-            B: tx_data[1] <= rx_data[1];
-            C: tx_data[2] <= rx_data[2];
-            D: tx_data[3] <= rx_data[3];
-            default: begin // basically IDLE
-                tx_data[0] <= 8'h0;
-                tx_data[1] <= 8'h0;
-                tx_data[2] <= 8'h0;
-                tx_data[3] <= 8'h0;
-            end
-        endcase
     end
 end
+
 
 
 
